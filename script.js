6a62341536f508b54f945cd509ed837a7745c6f4
Gorakh Raj Joshi <gorakh626@gmail.com>
2021-11-30 22:20:07 +0545

    Chaining Promises

--- script.js
+++ script.js
@@ -23,15 +23,20 @@ const renderCountry = function (data, className = '') {
 };
 
 const getCountryData = function (country) {
+  // Country 1
   fetch(`https://restcountries.com/v2/name/${country}`)
-    .then(function (response) {
-      console.log(response);
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
+    .then((response) => response.json())
+    .then((data) => {
       renderCountry(data[0]);
-    });
+      const neighbour = data[0].borders[0];
+
+      if (!neighbour) return;
+
+      // Country 2
+      return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
+    })
+    .then((response) => response.json())
+    .then((data) => renderCountry(data, 'neighbour'));
 };
 
 getCountryData('nepal');
