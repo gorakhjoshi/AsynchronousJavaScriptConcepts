7b575aafc1f05656c54e1404a47dea7426be2570
Gorakh Raj Joshi <gorakh626@gmail.com>
2021-11-30 22:46:42 +0545

    Running promises in parallel

--- script.js
+++ script.js
@@ -3,65 +3,33 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
-const renderCountry = function (data, className = '') {
-  const html = `
-  <article class="country ${className}">
-    <img class="country__img" src="${data.flag}" />
-    <div class="country__data">
-      <h3 class="country__name">${data.name}</h3>
-      <h4 class="country__region">${data.region}</h4>
-      <p class="country__row"><span>👫</span>${(
-        +data.population / 1000000
-      ).toFixed(1)} people</p>
-      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-    </div>
-  </article>
-  `;
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
+const getJSON = function (url, errorMsg = 'Something went wrong') {
+  return fetch(url).then((response) => {
+    if (!response.ok) throw new Error(`${errorMsg} ${response.status}`);
+    return response.json();
+  });
 };
 
-const renderError = function (msg) {
-  countriesContainer.insertAdjacentText('beforeend', msg);
-  countriesContainer.style.opacity = 1;
-};
-
-const getCountryData = async function (country) {
+const get3Countries = async function (c1, c2, c3) {
   try {
-    const resCountry = await fetch(
-      `https://restcountries.com/v2/name/${country}`
-    );
-    const data = await resCountry.json();
-    renderCountry(data[0]);
-
-    const neighbour = data[0].borders[0];
-
-    if (!neighbour) return new Error('No neighbour found');
-
-    const resNeighbour = await fetch(
-      `https://restcountries.com/v2/alpha/${neighbour}`
-    );
-    renderCountry(await resNeighbour.json(), 'neighbour');
-
-    return `Successfully fetched data of ${country}`;
-  } catch (error) {
-    console.log('error');
-    throw error;
+    // const [data1] = await getJSON(
+    //   `https://restcountries.eu/rest/v2/name/${c1}`
+    // );
+    // const [data2] = await getJSON(
+    //   `https://restcountries.eu/rest/v2/name/${c2}`
+    // );
+    // const [data3] = await getJSON(
+    //   `https://restcountries.eu/rest/v2/name/${c3}`
+    // );
+    // console.log([data1.capital, data2.capital, data3.capital]);
+    const data = await Promise.all([
+      getJSON(`https://restcountries.com/v2/name/${c1}`),
+      getJSON(`https://restcountries.com/v2/name/${c2}`),
+      getJSON(`https://restcountries.com/v2/name/${c3}`),
+    ]);
+    console.log(data.map((d) => d[0].capital));
+  } catch (err) {
+    console.error(err);
   }
 };
-
-// getCountryData('nepal')
-//   .then((res) => console.log(res))
-//   .catch((error) => console.log(error))
-//   .finally(() => console.log('All data fetched'));
-
-(async function () {
-  try {
-    const city = await getCountryData('nepal');
-    console.log(city);
-  } catch (error) {
-    console.log(error);
-  }
-  console.log('All data fetched');
-})();
+get3Countries('nepal', 'australia', 'china');
