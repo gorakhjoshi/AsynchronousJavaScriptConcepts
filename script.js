64f204f450a17a546226a634c48af14f5f192b2d
Gorakh Raj Joshi <gorakh626@gmail.com>
2021-11-30 22:29:11 +0545

    Building a simple promise

--- script.js
+++ script.js
@@ -62,10 +62,59 @@
 //   getCountryData('nepal');
 // });
 
-console.log('Test Start');
-setTimeout(() => console.log('0 sec timer'), 1000);
-Promise.resolve('Resolved promise 1').then((res) => console.log(res));
-Promise.resolve('Resolved promise 2').then((res) => {
-  for (let i = 0; i < 10000; i++) console.log(res);
+// console.log('Test Start');
+// setTimeout(() => console.log('0 sec timer'), 1000);
+// Promise.resolve('Resolved promise 1').then((res) => console.log(res));
+// Promise.resolve('Resolved promise 2').then((res) => {
+//   for (let i = 0; i < 10000; i++) console.log(res);
+// });
+// console.log('Test end');
+
+const lotteryPromise = new Promise(function (resolve, reject) {
+  console.log('Lotter draw is happening 🔮');
+  setTimeout(function () {
+    if (Math.random() >= 0.5) {
+      resolve('You WIN 💰');
+    } else {
+      reject(new Error('You lost your money 💩'));
+    }
+  }, 2000);
 });
-console.log('Test end');
+lotteryPromise
+  .then((res) => console.log(res))
+  .catch((err) => console.error(err));
+
+// Promisifying setTimeout
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+wait(1)
+  .then(() => {
+    console.log('1 second passed');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('2 second passed');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('3 second passed');
+    return wait(1);
+  })
+  .then(() => console.log('4 second passed'));
+// setTimeout(() => {
+//   console.log('1 second passed');
+//   setTimeout(() => {
+//     console.log('2 seconds passed');
+//     setTimeout(() => {
+//       console.log('3 second passed');
+//       setTimeout(() => {
+//         console.log('4 second passed');
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
+Promise.resolve('abc').then((x) => console.log(x));
+Promise.reject(new Error('Problem!')).catch((x) => console.error(x));
