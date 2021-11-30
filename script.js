40dd69c8e7cfb33f3d34ba9545a4161b0be837e1
Gorakh Raj Joshi <gorakh626@gmail.com>
2021-11-30 22:22:39 +0545

    Handling Rejected Promises

--- script.js
+++ script.js
@@ -19,7 +19,12 @@ const renderCountry = function (data, className = '') {
   </article>
   `;
   countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
+  // countriesContainer.style.opacity = 1;
+};
+
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  // countriesContainer.style.opacity = 1;
 };
 
 const getCountryData = function (country) {
@@ -36,7 +41,14 @@ const getCountryData = function (country) {
       return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
     })
     .then((response) => response.json())
-    .then((data) => renderCountry(data, 'neighbour'));
+    .then((data) => renderCountry(data, 'neighbour'))
+    .catch((err) => {
+      console.log(`${err} 🔥🔥🔥`);
+      renderError(`Something went wrong. 🎆🎆🎆 ${err.message}. Try again`);
+    })
+    .finally(() => (countriesContainer.style.opacity = 1));
 };
 
-getCountryData('nepal');
+btn.addEventListener('click', function () {
+  getCountryData('nepal');
+});
